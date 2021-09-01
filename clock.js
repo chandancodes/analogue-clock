8560621173747d925151b17dac629f2c5af87743
Chandan Singh <chandancodes@gmail.com>
2021-09-01 18:55:57 +0530

    js udate

--- clock.js
+++ clock.js
@@ -3,18 +3,19 @@ var ctx = canvas.getContext("2d");
 var radius = canvas.height / 2;
 ctx.translate(radius, radius);
 radius = radius * 0.90
-drawClock();
+setInterval(drawClock, 1000);
 
 function drawClock(){
     drawFace(ctx, radius);
     drawNumbers(ctx, radius);
+    drawTime(ctx, radius);
 }
 
 function drawFace(ctx, radius){
     var grad;
     ctx.beginPath();
     ctx.arc(0,0,radius,0,2*Math.PI);
-    ctx.fillStyle = "white";
+    ctx.fillStyle = 'white';
     ctx.fill();
     grad = ctx.createRadialGradient(0,0,radius*0.95,0,0,radius*1.05);
     grad.addColorStop(0, '#333');
@@ -50,7 +51,7 @@ function drawNumbers(ctx, radius){
 function drawTime(ctx, radius){
     var now = new Date();
     var hour = now.getHours();
-    var minute = now.getMintutes();
+    var minute = now.getMinutes();
     var second = now.getSeconds();
     //hour
     hour = hour%12;
@@ -58,4 +59,21 @@ function drawTime(ctx, radius){
     (minute*Math.PI/(6*60))+
     (second*Math.Pi/(360*60));
     drawHand(ctx, hour, radius*0.5, radius*0.07);
+    //minute
+    minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
+    drawHand(ctx, minute, radius*0.8, radius*0.07);
+    //second
+    second=(second*Math.PI/30);
+    drawHand(ctx, second, radius*0.9, radius*0.02);
+}
+
+function drawHand(ctx, pos, length, width){
+    ctx.beginPath();
+    ctx.lineWidth = width;
+    ctx.lineCap = "round";
+    ctx.moveTo(0,0);
+    ctx.rotate(pos);
+    ctx.lineTo(0, -length);
+    ctx.stroke();
+    ctx.rotate(-pos);
 }
